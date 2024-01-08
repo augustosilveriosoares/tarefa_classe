b91297f0acaf1c06cb575307d9be4b3966aa5273
Augusto Silvério Soares <falecomsilverio@gmail.com>
2024-01-08 18:09:48 -0300

    tarefa_controle_fluxo

--- src/br/com/ebac/CalculaMedia.java
+++ src/br/com/ebac/CalculaMedia.java
@@ -9,9 +9,11 @@ public class CalculaMedia {
 
     public static  void calcula(double nota1 ,double nota2, double nota3, double nota4){
         double media = (nota1 + nota2 + nota3 + nota4) / 4;
-        if (media > 7) {
+        if (media >= 7) {
             System.out.println("Média: " + String.format("%.2f", media) + " - Aprovado");
-        } else {
+        } else if(media >= 5) {
+            System.out.println("Média: " + String.format("%.2f", media) + " - Recuperação");
+        }else{
             System.out.println("Média: " + String.format("%.2f", media) + " - Reprovado");
         }
     }
